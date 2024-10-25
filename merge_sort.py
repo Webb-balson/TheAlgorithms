ac4bf34a60976d78dd517abd8b0c344248880604
Webb-balson <balsongorai@gmail.com>
2024-10-25 23:43:00 +0530

    Added merge sort implementation

---
+++ merge_sort.py
@@ -0,0 +1,42 @@
+# Python Program to implement Merge Sort
+"""
+Time Complexity: O(nlogn)
+Auxilary Space: O(n)
+"""
+
+def merge(left, right):
+    new = []
+    i,j = 0,0
+
+    while i< len(left) and j < len(right):
+        if left[i]<right[j]:
+            new.append(left[i])
+            i += 1
+        else:
+            new.append(right[j])
+            j += 1
+
+    new.extend(left[i:])
+    new.extend(right[j:])
+
+    return new
+
+def merge_sort(arr):
+
+    if len(arr) <= 1:
+        return arr
+    
+    mid = len(arr)//2
+
+    l_half = arr[:mid]
+    r_half = arr[mid:]
+
+    l_half = merge_sort(l_half)
+    r_half = merge_sort(r_half)
+
+    return merge(l_half, r_half)
+
+if __name__ == "__main__":
+    arr = [12,11,13,5,6,7]
+    result = merge_sort(arr)
+    print(f"The sorted array: {result}")
